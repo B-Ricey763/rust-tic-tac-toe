bcd765c3e2d25f5ce6f7da6962c0771859e9b9b7
B-Ricey763 <39998963+B-Ricey763@users.noreply.github.com>
2021-09-04 21:10:59 -0400

    Nice

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,75 @@
-use std::error::Error;
+use std::{error::Error, env, thread, time::Duration};
+use std::io::{self, Write, stdout};
+use crossterm::{
+    ExecutableCommand,
+    terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
+    cursor,
+    event::{self, Event, KeyCode}
+};
 
-fn run() -> Result<(), Box<dyn Error>> {
+
+/* 
+#[derive(Copy, Clone)]
+enum Tile {
+    // Enum variants can also have values
+    Empty = 0,
+    Circle = 1,
+    Cross = -1,
+}
+
+let tiles = [Tile::Empty, Tile::Empty, Tile::Cross];
+tiles.iter().map(|tile| *tile as i32).sum::<i32>()
+*/
+const MAX_LEN: usize = 3;
+
+pub struct Config {
+    pub single_player: bool,
+}
+
+impl Config {
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+        args.next();
+
+        // this really should be a flag, but right now
+        // if they put an arg, its multiplayer
+        let single_player = args.next().is_none();
+        
+        Ok(Config{ single_player })
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+enum Tile {
+    Empty = 0,
+    X = 1,
+    O = -1,
+}
+
+type Board = [[Tile; MAX_LEN]; MAX_LEN];
+
+pub fn run() -> Result<(), Box<dyn Error>> {
+    let mut stdout = stdout();
+    let mut board: Board = [[Tile::Empty; 3]; 3];
+    stdout.execute(EnterAlternateScreen)?;
+    terminal::enable_raw_mode()?;
+    stdout.lock();
+
+    stdout.execute(cursor::MoveTo(5, 5));
+    write!(&mut stdout, "Hello World!")?;
+    stdout.flush();
+
+    loop {
+        if let Event::Key(event) = event::read()? {
+            match event.code 
+                KeyCode::Up => println!("UP"),
+                KeyCode::Esc => break,
+                _ => (),
+            }
+        }
+    }
+    
+    terminal::disable_raw_mode()?;
+    stdout.execute(LeaveAlternateScreen)?;
     Ok(())
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::{process, env};
+
+use tic_tac_toe::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let _config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = tic_tac_toe::run() {
+        eprintln!("Game error: {}", e);
+        process::exit(1);
+    }
 }
